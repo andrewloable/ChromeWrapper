14e9a57b08fdad7d673e360479f2173196a706f6
Andrew Loable <andrew.loable@engagis.com>
2019-11-13 15:37:01 +0800

    Initial Commit

---
+++ ChromeWrapper/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChromeWrapper
+{
+    public class UI
+    {
+        public static void New(string url, int width, int height, bool waitForExit = false, List<string> customArgs = null)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                url = "data:text/html,<html></html>";
+
+            List<string> args = Chrome.DefaultChromeArgs();
+            args.Add($"-app=\"data:text/html,<html><body><script>window.resizeTo({width},{height});window.location='{url}';</script></body></html>\"");
+            if (customArgs != null)
+                args.AddRange(customArgs);
+
+            Chrome.NewChromeWithArgs(Chrome.LocateChrome(), args, waitForExit);
+        }
+    }
+}
---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ChromeWrapper;
+using System;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.New("https://loable.tech", 400, 400, true);
+        }
+    }
+}
